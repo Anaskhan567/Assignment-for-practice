bfaea5e67f78ab8e4c3c52ec91c1bfc361aef2e2
Anaskhan567 <ma3799372@gmail.com>
2024-06-07 18:09:08 -0700

    Add files via upload

---
+++ Assignment 3/main.js
@@ -0,0 +1,5 @@
+// Assignment 3
+// storing a famous quote and its author
+var quote = "nothing is imposssible because impossible itself I M possible";
+var author = 'Anas khan';
+console.log("\"".concat(quote, "\" - ").concat(author));
---
+++ Assignment 3/main.ts
@@ -0,0 +1,6 @@
+    // Assignment 3
+// storing a famous quote and its author
+
+let quote:string="nothing is imposssible because impossible itself I M possible";
+let author:string='Anas khan';
+console.log(`"${quote}" - ${author}`);
